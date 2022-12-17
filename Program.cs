7908a7cb1200991ec9131e6795a464e58470c72f
OVGI <girichevao@gmail.com>
2022-12-17 21:38:07 +0300

    add task4 zad 22

--- Program.cs
+++ Program.cs
@@ -1,20 +1,11 @@
-﻿/* Задача 20. Напишите программу , которая принимает на вход координаты двух точек
- и находит расстояние между ними в 2D пространстве.  
- A (3,6); B (2,1) - > 5,09
- A (7, -5); B (1, -1) - > 7,21     */
-int xa, xb, ya, yb;
+﻿/* Задача 22. Напишите программу , которая принимает на вход число (N) и выдает 
+таблицу квадратов чисел от 1 до N.  
+5 -> 1, 4, 9, 16, 25
+2 -> 1, 4  */
+int n;
 
-Console.Write($"Введите координату X точки A: ");
-int.TryParse(Console.ReadLine()!, out xa);
-Console.Write($"Введите координату Y точки A: ");
-int.TryParse(Console.ReadLine()!, out ya);
-Console.Write($"Введите координату X точки B: ");
-int.TryParse(Console.ReadLine()!, out xb);
-Console.Write($"Введите координату Y точки B: ");
-int.TryParse(Console.ReadLine()!, out yb);
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out n);
 
-int x = (xa - xb) * (xa - xb);
-int y = (ya - yb) * (ya - yb);
-
-double ans = Math.Sqrt(x + y);
-Console.Write($"Расстояние между точками A и B = {Math.Round(ans, 2)}");
+for (int i = 1; i <= n; i++)
+    Console.Write($"{i * i} ");
